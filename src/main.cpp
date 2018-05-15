f56dc759e906626faad30e7c234a7140408a9af9
Faisal Usmani <faisal.usmani@mail.utoronto.ca>
2018-05-14 20:08:22 -0400

    Added: the suggested changes.

--- src/main.cpp
+++ src/main.cpp
@@ -143,7 +143,7 @@ int main() {
           auto vars = mpc.Solve(state, coeffs);
           
           // Calculate steering and throttle
-          double steer_value = vars[0] / (deg2rad(25));
+          double steer_value = -1 * vars[0] / (deg2rad(25));
           double throttle_value = vars[1];
           
           // Send values to the simulator
